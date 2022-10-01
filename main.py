e46244e2ac9f040de2faed29a2e0097c58235932
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-01 08:39:40 -0300

    feat: remember that paths cant be redefined

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ async def read_item(item_id: int):
     return {"item_id": item_id}
 
 
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
+
+
+@app.get("/users")
+async def read_users2():
+    return ["Bean", "Elfo"]
+
+
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
@@ -21,3 +31,5 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+
