3778937b37998ab8121320dbda80616818a573f3
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-01 09:05:17 -0300

    add fake db and query param get route example

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 app = FastAPI()
 
@@ -14,6 +16,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "hello world"}
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 
 @app.get("/items/{item_id}")
