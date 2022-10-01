df1ba1bded1f615045db4c49193c10f2fddce9a3
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-01 09:40:59 -0300

    format file

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from enum import Enum
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
@@ -21,6 +29,11 @@ async def root():
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
 
 
 @app.get("/items/{item_id}")
