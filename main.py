6f607807795c09ce064359cb85ee2765f5a0d4ba
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-01 09:13:49 -0300

    get route with more than one param

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ async def read_user_me():
 async def read_user(user_id: str):
     return {"user_id": user_id}
 
+
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
@@ -64,6 +65,21 @@ async def get_model(model_name: ModelName):
 
     return {"model_name": model_name, "message": "Have some residuals"}
 
+
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
