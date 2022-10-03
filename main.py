6fa4b0f383f3dfed553a3f4693ce6343c7e97d9d
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-03 15:23:31 -0300

    refactor: put route to use query param

--- main.py
+++ main.py
@@ -40,8 +40,11 @@ async def create_item(item: Item):
     return item_dict
 
 @app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item):
-    return {"item_id": item_id, **item.dict()}
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
 
 
 @app.get("/items/{item_id}")
