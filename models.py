aa5cf66fbcb91b0b8a83ea12c5f13b0a9ff40621
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-03 17:48:14 -0300

    add index to name column

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from sqlmodel import Field, SQLModel, create_engine, Session, select
 
 class Hero(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
+    name: str = Field(index=True)
     secret_name: str
     age: Optional[int] = None
 
