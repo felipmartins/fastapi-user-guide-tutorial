a826e24b6bc65b164c2a9b936a9b2fb909b17e4c
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-03 16:39:53 -0300

    move create_all to function

--- models.py
+++ models.py
@@ -13,6 +13,8 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo=True)
 
-SQLModel.metadata.create_all(engine)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 # More code here later 👇
