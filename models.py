662bfda4565a79d79256090077e2dfb2a9196699
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-03 16:53:52 -0300

    add create heroes script to add to db

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, SQLModel, create_engine, Session
 
 
 class Hero(SQLModel, table=True):
@@ -17,5 +17,24 @@ engine = create_engine(sqlite_url, echo=True)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-if __name__ == "__main__":  # 
-    create_db_and_tables() 
+
+def create_heroes():
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+
+    session = Session(engine)
+
+    session.add(hero_1)
+    session.add(hero_2)
+    session.add(hero_3)
+
+    session.commit()
+
+
+def main():
+    create_db_and_tables()
+    create_heroes()
+
+if __name__ == "__main__":
+    main()
